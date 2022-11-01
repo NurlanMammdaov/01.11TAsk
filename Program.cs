bd8186abc6b376b8e5342218edf6f7c9189d420d
Nurlan Memmedov <nurlan.memmedov8818@gmail.com>
2022-11-01 23:15:18 +0400

    redy task

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//Task1
+//int[] numberArray = { 1, 2, 3, 4, 5, 6, 7, -1, 354 };
+//int count = 0;
+//for (int i = 0; i < numberArray.Length; i++)
+//{
+//    if (numberArray[i] % 6 == 0 && numberArray[i] > 0)
+//    {
+//        count += numberArray[i];
+//    }
+//}
+//Console.WriteLine(count);
+
+//TAsk2
+//int[] numberArray = { 1, 2, 3, 4, 5, 6, 7, -1, 354 };
+//int maxValue = numberArray[0];
+//for(int i = 0; i < numberArray.Length; i++)
+//{
+//    if(numberArray[i] > maxValue)
+//    {
+//        maxValue = numberArray[i];
+//    }
+//}
+//Console.WriteLine(maxValue);
---
+++ script.js
@@ -0,0 +1,67 @@
+//Task1
+// var numberArray = [ 1, 2, 3, 4, 5, 6, 7, -1, 354 ];
+// var a =0;
+// for(var i = 0; i < numberArray.Length; i++)
+// {
+//    if (numberArray[i]%6==0 && numberArray[i] > 0)
+//    {
+//        a=a+numberArray[i]
+//     }
+// }
+// alert(a)
+
+//Task2
+// var numberArray = [ 1, 2, 3, 4, 5, 6, 7, -1, 354 ];
+// var maxValue = numberArray[0];
+// for( i = 0; i < numberArray.Length; i++)
+// {
+//     if(numberArray[i] > maxValue)
+//     {
+//         var maxValue = numberArray[i];
+//    }
+// }
+// alert(maxValue)
+
+// TAsk3
+// var car=[
+//     {
+//         brend:"BMW",
+//         model:"X6",
+//         color:"red",
+//         year:2000
+
+//     },
+//     {
+//         brend:"Mercedes",
+//         model:"C250",
+//         color:"Black",
+//         year:2000
+
+//     }   ,
+//      {
+//         brend:"BMW",
+//         model:"X5",
+//         color:"White",
+//         year:2015
+
+//     }   ,
+//      {
+//         brend:"Mercedes",
+//         model:"E300",
+//         color:"grey",
+//         year:2000
+
+//     }  ,
+//       {
+//         brend:"Lada",
+//         model:"Niva",
+//         color:"Blue",
+//         year:2000
+
+//     }
+// ]
+// for(var i = 0;i<car.length;i++){
+//     if(car[i].year > 2010){
+//         console.log(car[i])
+//     }
+// }
